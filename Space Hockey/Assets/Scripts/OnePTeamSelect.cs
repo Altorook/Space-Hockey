e41d11faf70c139b4b8746de6ac2c153c88dc4db
Altorook <nathanberry403@gmail.com>
2024-10-03 22:32:56 -0600

    Auto stash before merge of "Smooztime" and "origin/Smooztime"

--- Space Hockey/Assets/Scripts/OnePTeamSelect.cs
+++ Space Hockey/Assets/Scripts/OnePTeamSelect.cs
@@ -66,7 +66,7 @@ public class OnePlayerTeamSelect : MonoBehaviour
         {
             savePlayers.p1Team = p1ActiveTeam;
             inputControl.DisableMenuCtrl();
-            SceneManager.LoadScene("ScriptTesting");
+            SceneManager.LoadScene("OnePlayerPlay");
         }
     }
 }
