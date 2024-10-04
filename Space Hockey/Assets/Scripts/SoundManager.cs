0c0042f335dea5d39b1ea7759a70d802eeae2c63
sayed akid <samsayed07866@gmail.com>
2024-10-03 18:17:28 -0600

    added sound and couple of chgnages

---
+++ Space Hockey/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource musicSource;
+
+    [SerializeField] private List<AudioClip> soundEffects;
+    [SerializeField] private List<AudioClip> musicTracks;
+
+    [Range(0f, 1f)] public float sfxVolume = 1f;
+    [Range(0f, 1f)] public float musicVolume = 1f;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+
+        sfxSource.volume = sfxVolume;
+        musicSource.volume = musicVolume;
+    }
+
+    public void PlaySFX(string clipName)
+    {
+        AudioClip clip = soundEffects.Find(sfx => sfx.name == clipName);
+        if (clip != null)
+        {
+            sfxSource.PlayOneShot(clip, sfxVolume);
+        }
+        else
+        {
+            Debug.LogWarning($"SFX '{clipName}' not found!");
+        }
+    }
+
+    public void PlayMusic(string clipName)
+    {
+        AudioClip clip = musicTracks.Find(music => music.name == clipName);
+        if (clip != null)
+        {
+            musicSource.clip = clip;
+            musicSource.volume = musicVolume;
+            musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"Music track '{clipName}' not found!");
+        }
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        sfxSource.volume = sfxVolume;
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        musicSource.volume = musicVolume;
+    }
+
+    public void FadeOutMusic(float fadeDuration)
+    {
+        StartCoroutine(FadeOutMusicCoroutine(fadeDuration));
+    }
+
+    private IEnumerator FadeOutMusicCoroutine(float fadeDuration)
+    {
+        float startVolume = musicSource.volume;
+
+        while (musicSource.volume > 0)
+        {
+            musicSource.volume -= startVolume * Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+
+        musicSource.Stop();
+        musicSource.volume = startVolume;
+    }
+}
